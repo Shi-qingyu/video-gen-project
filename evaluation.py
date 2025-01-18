45b91a9b4c94e29d35c9e6e02ed20493b5746128
sqy <q.y.shi@stu.pku.edu.cn>
2025-01-18 03:39:22 +0000

    eval

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-import json
-import torch
 
-from transformers import CLIPModel, CLIPProcessor
+import torch
+import torch.nn.functional as F
+
+from transformers import CLIPModel, CLIPProcessor, ViTImageProcessor, ViTModel
 
 
 def calculate_clip(model, processor, text, images_or_path):
@@ -23,14 +24,48 @@ def calculate_clip(model, processor, text, images_or_path):
     inputs["input_ids"] = inputs["input_ids"].to(model.device)
     inputs["attention_mask"] = inputs["attention_mask"].to(model.device)
     outputs = model(**inputs)
-    logits_per_image = outputs.logits_per_image
+    logits_per_image = torch.diagonal(outputs.logits_per_image)
     return logits_per_image
+
+
+def calculate_dino(model, processor, video_or_path):
+    if isinstance(video_or_path, Path):
+        video_or_path = video_or_path.as_posix()
+    
+    if isinstance(video_or_path, str):
+        import decord
+        decord.bridge.set_bridge("torch")
+
+        video_reader = decord.VideoReader(video_or_path)
+        video = video_reader.get_batch(list(range(len(video_reader))))
+        video = video.permute(0, 3, 1, 2) # [F, 3, H, W]
+
+    inputs = processor(video, return_tensors="pt")
+    inputs["pixel_values"] = inputs["pixel_values"].to(model.device)
+    outputs = model(**inputs)
+    pooler_outputs = outputs.pooler_output # [F, D]
+    pooler_outputs = F.normalize(pooler_outputs, p=2, dim=-1)
+
+    first_image_feature = pooler_outputs[[0]].repeat(len(video), 1)
+
+    prev_image_ids = torch.arange(0, len(video)) - 1
+    prev_image_ids[0] = 0
+    prev_image_feature = pooler_outputs[prev_image_ids] # [F, D]
+
+    first_image_sim = torch.einsum("f c, l c->f l", pooler_outputs, first_image_feature)    
+    prev_image_sim = torch.einsum("f c, l c->f l", pooler_outputs, prev_image_feature)
+
+    first_image_sim = torch.diagonal(first_image_sim).mean()
+    prev_image_sim = torch.diagonal(prev_image_sim).mean()
+
+    return (first_image_sim + prev_image_sim) / 2
 
 
 def clip_score(root="", device="cuda"):
     model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
     model = model.to(device)
+    model.eval()
     clip_score = 0
     cnt = 0
 
@@ -41,19 +76,34 @@ def clip_score(root="", device="cuda"):
             for video in data.iterdir():
                 if video.is_file() and video.suffix.endswith("mp4"):
                     logits_per_image = calculate_clip(model, processor, prompt, video)
-                    clip_score += logits_per_image.mean()
+                    clip_score += logits_per_image.mean().item()
                     cnt += 1
     
-    return clip_score.item() / cnt
+    return clip_score / cnt
 
 
 def dino_score(root="", device="cuda"):
-    pass
+    model = ViTModel.from_pretrained("facebook/dino-vitb16")
+    processor = ViTImageProcessor.from_pretrained("facebook/dino-vitb16")
+    model = model.to(device)
+    model.eval()
+    dino_score = 0
+    cnt = 0
 
+    root = Path(root)
+    with torch.no_grad():
+        for data in root.iterdir():
+            for video in data.iterdir():
+                if video.is_file() and video.suffix.endswith("mp4"):
+                    dino_score_per_video = calculate_dino(model, processor, video)
+                    dino_score += dino_score_per_video.item()
+                    cnt += 1
+
+    return dino_score / cnt
 
 def motion_fidelity_score():
     pass
 
 
 if __name__ == "__main__":
-    print(clip_score(root="outputs_benchmark", device="cuda:2"))
+    print(dino_score(root="outputs_benchmark", device="cuda"))
